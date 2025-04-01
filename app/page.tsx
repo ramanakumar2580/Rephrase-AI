b3babc299aae66e878cf43ec59ae3fd7e960eaee
Manupati Ramana Kumar <manupatiramanakumar106@gmail.com>
2025-04-02 01:12:57 +0530

    Add Header and HeroSection components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import BgGradient from "@/components/common/bg-gradient";
+import HeroSection from "@/components/home/hero-section";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
     </div>
   );
 }
